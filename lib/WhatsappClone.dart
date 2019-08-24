906c9c2500178e46614a3e4bfb6d27001464e842
Sanni Prasad <prasadsunny1@gmail.com>
2019-08-24 23:57:13 +0530

    Add Whatsapp clone place holder nav

---
+++ lib/WhatsappClone.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class WhatsappClone extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("WhatsappClone"),
+      ),
+      body: Center(child: Text("Whatsapp Clone")),
+    );
+  }
+}
