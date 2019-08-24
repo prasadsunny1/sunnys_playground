842c133fc19fce280cad46d704511f3d8968f492
Sanni Prasad <prasadsunny1@gmail.com>
2019-08-25 00:17:34 +0530

    Basic whatsapp listview

--- lib/WhatsappClone.dart
+++ lib/WhatsappClone.dart
@@ -9,7 +9,27 @@ class WhatsappClone extends StatelessWidget {
         appBar: AppBar(
           title: Text("WhatsappClone"),
         ),
-        body: Center(child: Text("Whatsapp Clone")),
+        body: ListView.builder(
+            itemCount: 10,
+            itemBuilder: (context, i) {
+              return new ListTile(
+                title: Text(
+                  "Sunny",
+                  style: Theme.of(context).textTheme.title,
+                ),
+                subtitle: Text(
+                  "Kya bhai,kaisa chal raha hai.Mauj hai ki aise hi bakhat kat raha hai",
+                  style: Theme.of(context).textTheme.body1,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                leading: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      "https://avatars3.githubusercontent.com/u/11705392?s=460&v=4"),
+                ),
+                trailing: Text("Yesterday"),
+              );
+            }),
       ),
     );
   }
