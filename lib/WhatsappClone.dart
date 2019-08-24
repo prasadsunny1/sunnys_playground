d7bec3186e610d1dd9123af70567ac87fb6e3ee5
Sanni Prasad <prasadsunny1@gmail.com>
2019-08-25 00:01:37 +0530

    Set theme for whatsapp

--- lib/WhatsappClone.dart
+++ lib/WhatsappClone.dart
@@ -3,11 +3,14 @@ import 'package:flutter/material.dart';
 class WhatsappClone extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("WhatsappClone"),
+    return Theme(
+      data: Theme.of(context).copyWith(primaryColor: Colors.green),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("WhatsappClone"),
+        ),
+        body: Center(child: Text("Whatsapp Clone")),
       ),
-      body: Center(child: Text("Whatsapp Clone")),
     );
   }
 }
