be8d40b3fc556d8f8e0cd0989fe8cac5eb64ddf8
sanni prasad <prasadsunny1@gmail.com>
2020-08-08 17:54:21 +0530

    Update flutter project

--- android/app/src/main/kotlin/dev/prasadsunny1/sunnys_playground/MainActivity.kt
+++ android/app/src/main/kotlin/dev/prasadsunny1/sunnys_playground/MainActivity.kt
@@ -1,13 +1,6 @@
 package dev.prasadsunny1.sunnys_playground
 
-import android.os.Bundle
-
-import io.flutter.app.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-  }
 }
