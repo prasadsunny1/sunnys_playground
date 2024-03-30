f6faa1bdf11f5b678a232108a6bff02432a6b92b
Sanni Prasad <prasadsunny1@gmail.com>
2024-03-30 18:02:18 +0530

    Create new project

--- android/app/src/main/kotlin/dev/prasadsunny1/sunnys_playground/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package dev.prasadsunny1.sunnys_playground
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/dev/prasadsunny1/sunnys_playground/sunnys_playground/MainActivity.kt
@@ -0,0 +1,5 @@
+package dev.prasadsunny1.sunnys_playground.sunnys_playground
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
